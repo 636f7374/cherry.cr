e5914a65a99a8a73535c430abf93fad22ae31edd
_ <53088156+636f7374@users.noreply.github.com>
2020-12-17 16:05:10 +0800

    Make sure that MITM::Client.Upgrade return OpenSSL Context.

--- src/cherry/mitm/client.cr
+++ src/cherry/mitm/client.cr
@@ -1,11 +1,11 @@
 class MITM::Client
   def self.upgrade(io : IO, hostname : String? = nil, options : Array(LibSSL::Options)? = nil,
-                   verify_mode : OpenSSL::SSL::VerifyMode = OpenSSL::SSL::VerifyMode::NONE)
-    return io unless client_context = Context.create_client
+                   verify_mode : OpenSSL::SSL::VerifyMode = OpenSSL::SSL::VerifyMode::NONE) : Tuple(OpenSSL::SSL::Context::Client?, IO)
+    return Tuple.new nil, io unless client_context = Context.create_client
     options.try &.each { |option| client_context.add_options option } rescue nil
 
     upgrade = OpenSSL::SSL::Socket::Client.new io: io, context: client_context, sync_close: true, hostname: hostname rescue nil
 
-    upgrade || io
+    Tuple.new client_context, (upgrade || io)
   end
 end
